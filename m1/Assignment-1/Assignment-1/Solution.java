16dd1fb303426f89023a0d0bd4dd33adbf9193d0
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-08-28 20:15:13 +0530

    Solution.java

---
+++ m1/Assignment-1/Assignment-1/Solution.java
@@ -0,0 +1,22 @@
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**.
+         * { item_description }
+         */
+        System.out.println("Name : K.Viswas varma, Roll Number : 20186039");
+    }
+}
