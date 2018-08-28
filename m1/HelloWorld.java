859ea3febd08a6605b062128b2b2ab9b63f53840
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-08-28 14:26:43 +0530

    HelloWorld.java

---
+++ m1/HelloWorld.java
@@ -0,0 +1,21 @@
+/**.
+Author: Viswas
+Date: 28-8-2018
+*/
+public final class HelloWorld {
+    /**.
+        *constructor
+    */
+    private HelloWorld() {
+        // zero argument constructor
+    }
+    /**.
+        *@param args of type string: command line argument
+    */
+    public static void main(final String[] args) {
+    /**.
+    main method
+    */
+        System.out.println("Hello World");
+    }
+}
