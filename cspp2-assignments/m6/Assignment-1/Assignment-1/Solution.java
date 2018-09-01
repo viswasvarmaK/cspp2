2dd3b53f2b0af5c6086470f65e9ade3998a6ae18
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-09-01 10:12:27 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-1/Assignment-1/Solution.java
+++ cspp2-assignments/m6/Assignment-1/Assignment-1/Solution.java
@@ -25,9 +25,9 @@ public final class Solution {
     			for(int j=0;j<=n;j++) {
     				if(i%j==0) {
     					count+=1;
-    			if(count!=2) {
-    				System.out.println(i);
-    					}
+    	if(count!=2) {
+    		System.out.println(i);
+    	}
     				}
     			}
     		}
