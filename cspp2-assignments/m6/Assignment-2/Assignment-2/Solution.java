a95a329eccacf27755ac4eab86be769d3ab1bd47
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-09-01 12:06:24 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-2/Assignment-2/Solution.java
+++ cspp2-assignments/m6/Assignment-2/Assignment-2/Solution.java
@@ -27,22 +27,31 @@ final class Solution {
             for (int j = 0 ; j < columns; j++) {
                 if (a[i][j] > 450) {
                     a[i][j] = 500;
+                    continue;
                 }else if(a[i][j] <= 450) {
                     a[i][j] = 400;
+                    continue;
                 }else if(a[i][j] >= 350) {
                     a[i][j] = 400;
+                    continue;
                 }else if(a[i][j] <= 350) {
                     a[i][j] = 300;
+                    continue;
                 }else if(a[i][j] >= 250) {
                     a[i][j] = 300;
+                    continue;
                 }else if(a[i][j] <= 250) {
                     a[i][j] = 200;
+                    continue;
                 }else if(a[i][j] >= 150) {
                     a[i][j] = 200;
+                    continue;
                 }else if(a[i][j] <= 150) {
                     a[i][j] = 100;
+                    continue;
                 }else if(a[i][j] >= 50) {
                     a[i][j] = 100;
+                    continue;
                 }else { 
                     a[i][j] = 0;
                     break;
