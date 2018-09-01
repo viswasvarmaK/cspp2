c8e8d91ea0cbfd99824e5141e7cba75525af636a
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-09-01 10:22:19 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-1/Assignment-1/Solution.java
+++ cspp2-assignments/m6/Assignment-1/Assignment-1/Solution.java
@@ -19,12 +19,12 @@ public final class Solution {
      */
     static void oddComposites(final int n) {
 	// write your code here
-    	for(int i=2;i<=n;i++) {
-    		if(i%2!=0) {
+    	for(int i = 2 ; i <= n; i++) {
+    		if(i % 2 != 0) {
     			int count = 0;
-    			for(int j = 1;j <= n; j++) {
-    				if(i%j == 0) {
-    					count= count + 1;
+    			for(int j = 1; j <= n; j++) {
+    				if(i % j == 0) {
+    					count = count + 1;
     				}
     			}
     			if(count != 2) {
