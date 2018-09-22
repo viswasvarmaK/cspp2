a137c921e6ab162cd924f416121634f0e4a4fb9c
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-09-22 12:48:05 +0530

    "check_style_score -> 11 of 14 passed. style score: 0/1 "

--- cspp2-assignments/m25/Assignment-1/Assignment-1/Task.java
+++ cspp2-assignments/m25/Assignment-1/Assignment-1/Task.java
@@ -1,49 +1,33 @@
+import java.util.Scanner;
+import java.util.Arrays;
 /**
  * Class for task.
  */
 class Task {
 	String title;
 	String assignedTo;
-	int  timeToComplete;
+	int timeToComplete;
 	boolean important;
 	boolean urgent;
-	String status;
-
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      title           The title
-	 * @param      assignedTo      The assigned to
-	 * @param      timeToComplete  The time to complete
-	 * @param      important       The important
-	 * @param      urgent          The urgent
-	 * @param      status          The status
-	 */
-	public Task(String title, String assignedTo, int  timeToComplete,
-	 boolean important, boolean urgent, String status) throws Exception {
-	 	if (title.equals("") || title == null) throw new Exception ("Title not provided");
-	 	if (timeToComplete < 0) throw new Exception ("Invalid timeToComplete" + timeToComplete);
-	 	if (!status.equals("todo") && !status.equals("done")) throw new Exception("Invalid status" + status);
-	 	this.title = title;
-	 	this.assignedTo = assignedTo;
-	 	this.timeToComplete = timeToComplete;
-	 	this.important = important;
-	 	this.urgent = urgent;
-	 	this.status = status;
-
+	String status;	
+	Task(String title, String assignedTo, int timeToComplete, boolean important,
+	     boolean urgent, String status) throws Exception {
+		if (title.equals("") || title == null) throw new Exception("Title not provided");
+		if (timeToComplete < 0) throw new Exception("Invalid timeToComplete " + timeToComplete);
+		if (!status.equals("todo") && !status.equals("done")) throw new Exception("Invalid status " + status);
+		this.title = title;
+		this.assignedTo = assignedTo;
+		this.timeToComplete = timeToComplete;
+		this.important = important;
+		this.urgent = urgent;
+		this.status = status;
 	}
-	/**
-	 * Returns a string representation of the object.
-	 *
-	 * @return     String representation of the object.
-	 */
 	public String toString() {
-		String impt = "Not Important";
-		String urgt = "Not Urgent";
-        if(important) impt = "Important";
-        if (urgent) urgt = "Urgent";
-		String display = title + "," + assignedTo + ","
-		 + timeToComplete + "," + impt + "," + urgt + "," + status;
-		 return display;
+		String imp = "Not Important";
+		String urg = "Not Urgent";
+		if(important) imp = "Important";
+		if(urgent) urg = "Urgent";
+		String display = title + ", " + assignedTo+", "+ timeToComplete+", "+ imp+", "+ urg+", "+ status;
+		return display;
 	}
-    }
+}
